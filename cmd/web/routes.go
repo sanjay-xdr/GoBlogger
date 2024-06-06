40f32e29d76a079f3f6f16d022027855741c8de8
Sanjay Jaiswal <sanjayjaiswal586@gmail.com>
2024-06-06 12:31:58 +0530

    adding routes and handlers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,5 +14,19 @@ func Routes() http.Handler {
 	mux.Use(loggerfunc)
 	mux.Get("/", handlers.HomePage)
 
+	//auth
+	mux.Get("/login", handlers.Login)
+	mux.Post("/login", handlers.PostLogin)
+	mux.Get("/signup", handlers.SignUp)
+	mux.Post("/signup", handlers.PostSignUp)
+
+	//users
+	mux.Get("/user/{id}", handlers.GetUserById)
+	mux.Post("/user/{id}", handlers.UpdateUserById)
+
+	//blogs
+	mux.Get("/blog", handlers.GetAllBlogs)
+	mux.Get("/blog/{id}", handlers.GetBlogById)
+
 	return mux
 }
