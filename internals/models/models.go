f56187bebc0a05787b153f555851f6aaa7c2b2fe
Sanjay Jaiswal <sanjayjaiswal586@gmail.com>
2024-06-13 12:56:17 +0530

    adding models

---
+++ internals/models/models.go
@@ -0,0 +1,30 @@
+package models
+
+import "time"
+
+type User struct {
+	FirstName string
+	LastName  string
+	Email     string
+	Password  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Blog struct {
+	Heading    string
+	SubHeading string
+	Content    string
+	UserId     int
+	CommentId  int
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
+
+type Comment struct {
+	Comment   string
+	UserId    int
+	BlogId    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
